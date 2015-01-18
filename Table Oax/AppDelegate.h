f686423176561d3bddbb49799056f2d1c6371216
g1sus <proyectos_technology@hotmail.com>
2015-01-18 09:27:44 -0600

    Initial Commit

---
+++ Table Oax/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Table Oax
+//
+//  Created by Jesus Antonio Hernandez Mendez on 18/01/15.
+//  Copyright (c) 2015 BTICs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Table Oax/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Table Oax
+//
+//  Created by Jesus Antonio Hernandez Mendez on 18/01/15.
+//  Copyright (c) 2015 BTICs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
